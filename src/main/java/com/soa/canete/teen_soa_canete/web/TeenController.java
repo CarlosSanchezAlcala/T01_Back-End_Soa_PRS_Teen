19f21464090d13edaada68d7f333e1683f7a96e2
Carlos Sanchez <carlos.sanchez@vallegrande.edu.pe>
2023-12-18 15:56:20 -0500

    [UPDATE] Securization

--- src/main/java/com/soa/canete/teen_soa_canete/web/TeenController.java
+++ src/main/java/com/soa/canete/teen_soa_canete/web/TeenController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/teenData")
 @RequiredArgsConstructor
 public class TeenController {
