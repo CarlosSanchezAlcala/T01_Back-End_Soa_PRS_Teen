5e4f56fe6293b17ef3667af5a680038fae741b1a
Carlos Sanchez <carlos.sanchez@vallegrande.edu.pe>
2024-02-20 21:08:20 -0500

    [UPDATE] Teen controller

--- src/main/java/com/soa/canete/teen_soa_canete/web/TeenController.java
+++ src/main/java/com/soa/canete/teen_soa_canete/web/TeenController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
